8e98d7549fd381b7e5a7a6b25676de3cf0076af8
AldegaArt <anyukhin.artem@gmail.com>
2023-08-01 22:13:56 +0300

    add Shuttle.toml file

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -6,10 +6,18 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/artem")]
+async fn hello_artem() -> &'static str {
+    "Hello Artem!"
+}
+
+#[get("/liza")]
+async fn hello_liza() -> &'static str {
+    "Hello Elizaveta!"
+}
+
 #[shuttle_runtime::main]
-async fn actix_web(
-    
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
